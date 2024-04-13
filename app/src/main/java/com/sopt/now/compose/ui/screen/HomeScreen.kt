90c277ab8d65f2e1c8e9816c7b10709e0e82f346
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-04-14 02:19:23 +0900

    [Feat] 2주차 compose 필수 과제 - 내프로필 itemlist 생성

--- app/src/main/java/com/sopt/now/compose/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/sopt/now/compose/ui/screen/HomeScreen.kt
@@ -3,11 +3,18 @@ package com.sopt.now.compose.ui.screen
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import com.sopt.now.compose.data.FriendProfileItem
-import com.sopt.now.compose.data.friendList
+import com.sopt.now.compose.ui.Item.FriendProfileItem
+import com.sopt.now.compose.ui.Item.MyProfileItem
+import com.sopt.now.compose.ui.Item.friendList
+import com.sopt.now.compose.ui.Item.myprofileList
 
 @Composable
 fun HomeScreen() {
+    LazyColumn {
+        items(myprofileList) {
+            MyProfileItem(it)
+        }
+    }
     LazyColumn {
         items(friendList) {
             FriendProfileItem(it)
