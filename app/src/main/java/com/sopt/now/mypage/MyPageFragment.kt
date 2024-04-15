3355f320c3ad30e73d3bddd2ba80398eae376e08
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-04-15 21:40:27 +0900

    [Feat] #5 add bottomnavigation

---
+++ app/src/main/java/com/sopt/now/mypage/MyPageFragment.kt
@@ -0,0 +1,20 @@
+package com.sopt.now.mypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sopt.now.databinding.FragmentMypageBinding
+
+class MyPageFragment : Fragment(){
+    private lateinit var binding: FragmentMypageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
