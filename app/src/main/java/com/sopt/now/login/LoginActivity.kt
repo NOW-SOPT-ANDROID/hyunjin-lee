aebb5ca056c0bfcaa9663d52ec7bc8adb115086c
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-04-19 04:10:33 +0900

    [Mod] #11 LoginActivity 및 뷰모델 수정

--- app/src/main/java/com/sopt/now/login/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/login/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.sopt.now.login
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
@@ -7,10 +8,13 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import android.util.Log
 import android.widget.CheckBox
+import android.widget.EditText
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.sopt.now.MyApplication
+import com.sopt.now.PreferenceUtil
 import com.sopt.now.R
 import com.sopt.now.signup.SignUpActivity
 import com.sopt.now.data.UserData
@@ -20,60 +24,31 @@ import com.sopt.now.main.MainActivity
 class LoginActivity : AppCompatActivity() {
     private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     private val viewModel: LoginViewModel by viewModels()
-    private lateinit var sharedPreferences: SharedPreferences
-
-    // 상수화
-    companion object {
-        const val USER_DATA = "user_data"
-        const val USER_ID = "user_id"
-        const val USER_PW = "user_pw"
-        const val USER_NAME = "user_name"
-        const val USER_MBTI = "user_mbti"
-        const val IS_AUTO_LOGIN = "is_auto_login"
-    }
-
-    // 회원가입 정보 받는 콜백
-    private val getSignupResult =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                result ->
-            if (result.resultCode == RESULT_OK) {
-                // 회원가입 성공 후 받은 사용자 정보를 SharedPreferences를 통해 저장
-                val user: UserData? = result.data?.getParcelableExtra(USER_DATA)
-                user?.let {
-                    with(sharedPreferences.edit()) {
-                        putString(USER_ID, it.id)
-                        putString(USER_PW, it.pw)
-                        putString(USER_NAME, it.name)
-                        putString(USER_MBTI, it.mbti)
-                        apply()
-                    }
-                    viewModel.setUserInfo(it)
-                }
-            }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        setUserData()// EditText에 데이터 설정
-        checkAutoLogin()
+        setUserData()
 
-        // setOnClickListener 설정
+        // 자동 로그인
+        loginObserver()
+        
+        // 로그인 버튼 클릭 시
         binding.btLoginButton.setOnClickListener {
             handleLoginClick()
         }
+        
+        // 회원가입 버튼 클릭 시
         binding.tvLoginSignupButton.setOnClickListener {
             handleSignupClick()
         }
     }
 
     private fun setUserData() {
-        viewModel.userInfo.observe(this) { user ->
-            binding.etLoginId.setText(user.id) // 로그인 화면에 사용자 ID를 자동으로 작성
-            binding.etLoginPw.setText(user.pw) // 로그인 화면에 사용자 PW를 자동으로 작성
-        }
+        val userData = viewModel.getUserInfo()
+        binding.etLoginId.setText(userData.id) // 로그인 화면에 사용자 ID를 자동으로 작성
+        binding.etLoginPw.setText(userData.pw) // 로그인 화면에 사용자 PW를 자동으로 작성
     }
 
     private fun handleLoginClick() {
@@ -82,46 +57,39 @@ class LoginActivity : AppCompatActivity() {
             binding.etLoginPw.text.toString()
         )
 
-        if (viewModel.loginValid(inputId, inputPw)) {
-            with(sharedPreferences.edit()) {
-                putString(USER_ID, inputId)
-                putString(USER_PW, inputPw)
-                putBoolean(IS_AUTO_LOGIN, true)
-                apply()
-            }
-
+        val userInfo = viewModel.getUserInfo()
+        if (viewModel.loginValid(inputId, inputPw, userInfo)) {
             Snackbar.make(binding.root, "로그인 성공", Snackbar.LENGTH_SHORT).show()
             navigateToMain()
         } else {
             Snackbar.make(binding.root, "로그인 실패", Snackbar.LENGTH_SHORT).show()
         }
-        finish()
     }
 
     private fun handleSignupClick() {
-        getSignupResult.launch(Intent(this@LoginActivity, SignUpActivity::class.java))
+        Intent(this@LoginActivity, SignUpActivity::class.java).let {
+            startActivity(it)
+        }
     }
 
-    private fun checkAutoLogin() {
-        val userId = sharedPreferences.getString(USER_ID, null)
-        val userPw = sharedPreferences.getString(USER_PW, null)
-        val userName = sharedPreferences.getString(USER_NAME, null)
-        val userMbti = sharedPreferences.getString(USER_MBTI, null)
-        val isAutoLogin = sharedPreferences.getBoolean(IS_AUTO_LOGIN, false)
-        Log.d("login", "$isAutoLogin")
-        if (userId != null && userPw != null && isAutoLogin) {
-            // 자동로그인 처리
-            val user = UserData(userId, userPw, userName.toString(), userMbti.toString(), 0)
-            viewModel.setUserInfo(user) // ViewModel 업데이트
-            navigateToMain()
+    private fun loginObserver() {
+        viewModel.userInfo.observe(this) {
+            if (viewModel.userInfo.value == true) {
+                Snackbar.make(binding.root, "로그인 성공", Snackbar.LENGTH_SHORT).show()
+                navigateToMain()
+            }
         }
     }
 
     private fun navigateToMain() {
-        val intent = Intent(this@LoginActivity, MainActivity::class.java).apply {
-            putExtra(USER_DATA, viewModel.userInfo.value)
+        Intent(this, MainActivity::class.java).let {
+            startActivity(it)
         }
-        startActivity(intent)
         finish()
+    }
+
+    // 상수화
+    companion object {
+        const val USER_DATA = "user_data"
     }
 }
--- app/src/main/java/com/sopt/now/login/LoginViewModel.kt
+++ app/src/main/java/com/sopt/now/login/LoginViewModel.kt
@@ -3,20 +3,42 @@ package com.sopt.now.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sopt.now.MyApplication
 import com.sopt.now.data.UserData
 
 class LoginViewModel : ViewModel(){
     // 로그인 정보를 담은 User를 LiveData로 관리
-    private val _userInfo = MutableLiveData<UserData>()
-    val userInfo: LiveData<UserData> = _userInfo
+    private val _userInfo: MutableLiveData<Boolean> = MutableLiveData(false)
+    val userInfo: LiveData<Boolean> = _userInfo
 
-    fun setUserInfo(user: UserData) {
-        _userInfo.value = user
+    init {
+        setUserInfo()
     }
 
-    fun loginValid(id: String, pw: String): Boolean {
-        return userInfo.value?.let {
-            it.id == id && it.pw == pw
-        } ?: false
+    private fun setUserInfo() {
+        if (MyApplication.prefs.getBoolean(LOGIN_STATE_KEY))
+            _userInfo.value = true
+    }
+
+    fun getUserInfo(): UserData {
+        return MyApplication.prefs.getUserData(PREF_KEY)
+    }
+
+    fun loginValid(id: String, pw: String, userData: UserData): Boolean {
+        when {
+            id.isBlank() -> return false
+            pw.isBlank() -> return false
+            id == userData.id && pw == userData.pw -> {
+                MyApplication.prefs.setBoolean(LOGIN_STATE_KEY, true)
+                setUserInfo()
+                return true
+            }
+            else -> return false
+        }
+    }
+
+    companion object {
+        private const val PREF_KEY = "userData"
+        private const val LOGIN_STATE_KEY = "true"
     }
 }
