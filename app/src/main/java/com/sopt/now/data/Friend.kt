ed973c87a46b94f8ea83441f75ed2fbea632e26a
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-04-25 04:15:36 +0900

    [Feat] #19 FriendDB 관련 파일 생성

--- app/src/main/java/com/sopt/now/data/Friend.kt
+++ app/src/main/java/com/sopt/now/data/Friend.kt
@@ -1,9 +1,14 @@
 package com.sopt.now.data
 
 import androidx.annotation.DrawableRes
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity
 data class Friend(
-    @DrawableRes val profileImage: Int,
-    val name: String,
-    val selfDescription: String,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @DrawableRes @ColumnInfo(name = "profile_image") val profileImage: Int,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "self_description") val selfDescription: String,
 )
