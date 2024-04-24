15ca80bc802958f6dffdccba1959ab68af22789a
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-04-25 04:16:47 +0900

    [Feat] #19 친구 삭제 dialog 기능 추가

--- app/src/main/java/com/sopt/now/home/FriendViewHolder.kt
+++ app/src/main/java/com/sopt/now/home/FriendViewHolder.kt
@@ -1,15 +1,49 @@
 package com.sopt.now.home
 
+import android.app.AlertDialog
+import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import com.sopt.now.R
 import com.sopt.now.data.Friend
 import com.sopt.now.databinding.ItemFriendBinding
 
-class FriendViewHolder(private val binding: ItemFriendBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(friendData: Friend) {
+class FriendViewHolder(private val binding: ItemFriendBinding, private val onFriendLongClick: (Friend) -> Unit) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(friend: Friend) {
+        // 바인딩 로직
         binding.run {
-            ivProfile.setImageResource(friendData.profileImage)
-            tvName.text = friendData.name
-            tvSelfDescription.text = friendData.selfDescription
+            ivProfile.setImageResource(friend.profileImage)
+            tvName.text = friend.name
+            tvSelfDescription.text = friend.selfDescription
         }
+
+        itemView.setOnLongClickListener {
+            dialogRemoveFriend(itemView.context, friend)
+            true
+        }
+    }
+
+    private fun dialogRemoveFriend(context: Context, friend: Friend) {
+        AlertDialog.Builder(context)
+            .setView(R.layout.dialog_removefriend)
+            .show()
+            .also { alertDialog ->
+                if (alertDialog == null) {
+                    return@also
+                }
+
+                val button_accept =
+                    alertDialog.findViewById<MaterialButton>(R.id.bt_removedialog_approve_button)
+                val button_cancel =
+                    alertDialog.findViewById<MaterialButton>(R.id.bt_removedialog_cancel_button)
+
+                button_accept?.setOnClickListener {
+                    alertDialog.dismiss()
+                    onFriendLongClick(friend)
+                }
+                button_cancel?.setOnClickListener {
+                    alertDialog.dismiss()
+                }
+            }
     }
 }
