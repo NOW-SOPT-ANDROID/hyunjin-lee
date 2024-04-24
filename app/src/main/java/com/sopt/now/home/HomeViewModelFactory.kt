2ab6e283e724a8466ccb0feef72b8fba84b90b34
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-04-25 04:20:29 +0900

    [Feat] #19 HomeViewModelFactory 파일 생성

---
+++ app/src/main/java/com/sopt/now/home/HomeViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.sopt.now.home
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class HomeViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return HomeViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
