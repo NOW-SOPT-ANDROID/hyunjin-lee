2ad0fc1a9c11e29d78ef18c0afff1a47e4bbff6b
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-04-26 04:37:50 +0900

    [Mod] #19 dataclass Entity로 변경

--- app/src/main/java/com/sopt/now/data/UserData.kt
+++ app/src/main/java/com/sopt/now/data/UserData.kt
@@ -1,14 +1,19 @@
 package com.sopt.now.data
 
 import android.os.Parcelable
-import androidx.annotation.DrawableRes
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity(tableName = "user_data")
 data class UserData(
-    val id: String,
-    val pw: String,
-    val name: String,
-    val mbti: String,
-    @DrawableRes val profileImage: Int,
-): Parcelable
+    @ColumnInfo(name = "user_id") val userid: String,
+    @ColumnInfo(name = "user_pw") val userpw: String,
+    @ColumnInfo(name = "user_name") val username: String,
+    @ColumnInfo(name = "user_mbti") val usermbti: String,
+    @ColumnInfo(name = "user_image") val userprofileImage: Int?,
+): Parcelable {
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+}
