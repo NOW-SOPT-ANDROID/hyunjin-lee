670bd6777821773a5130018f11545c4e05367d6f
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-04-26 04:36:03 +0900

    [Feat] #19 UserViewModelFactory 파일 생성

---
+++ app/src/main/java/com/sopt/now/main/UserViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.sopt.now.main
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class UserViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
