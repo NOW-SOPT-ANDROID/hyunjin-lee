0eb7b03b625d3904ceedc150c6c973f85abb0a61
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-04-27 00:30:32 +0900

    [Feat] #19 RetrofitClient 파일 생성

---
+++ app/src/main/java/com/sopt/now/retrofit/RetrofitClient.kt
@@ -0,0 +1,16 @@
+package com.sopt.now.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.github.com/"
+
+    val instance: GitHubService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(GitHubService::class.java)
+    }
+}
