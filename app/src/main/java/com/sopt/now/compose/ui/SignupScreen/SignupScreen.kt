cd918e316c169682a0592c7bc456e301f224f068
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-05-03 00:47:12 +0900

    [Feat] #27 SignupScreen API 연결

--- app/src/main/java/com/sopt/now/compose/ui/SignupScreen/SignupScreen.kt
+++ app/src/main/java/com/sopt/now/compose/ui/SignupScreen/SignupScreen.kt
@@ -1,5 +1,6 @@
 package com.sopt.now.compose.ui.SignupScreen
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,9 +12,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,8 +29,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.sopt.now.compose.R
 import com.sopt.now.compose.component.AppButton
-import com.sopt.now.compose.data.UserData
-
+import com.sopt.now.compose.data.auth.SignUpData.RequestSignUpDto
 
 @Composable
 fun SignupScreen(
@@ -36,10 +37,14 @@ fun SignupScreen(
     navController: NavController,
     signupViewModel: SignUpViewModel = viewModel()
 ) {
-    var userId by remember { mutableStateOf("") }
-    var userPw by remember { mutableStateOf("") }
-    var userName by remember { mutableStateOf("") }
-    var userMbti by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    var userId by rememberSaveable { mutableStateOf("") }
+    var userPw by rememberSaveable { mutableStateOf("") }
+    var userName by rememberSaveable { mutableStateOf("") }
+    var userPhone by rememberSaveable { mutableStateOf("") }
+
+    val signUpState by signupViewModel.signUpState.collectAsState()
 
     Column(
         modifier = modifier
@@ -55,8 +60,6 @@ fun SignupScreen(
             modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Center
         )
-
-        // 높이 30dp인 공간 추가
         Spacer(modifier = modifier.height(50.dp))
 
         // id
@@ -77,8 +80,6 @@ fun SignupScreen(
             placeholder = { Text(stringResource(id = R.string.ID_daisy)) },
             singleLine = true,
         )
-
-        // 높이 30dp인 공간 추가
         Spacer(modifier = modifier.height(30.dp))
 
         // pw
@@ -99,8 +100,6 @@ fun SignupScreen(
             placeholder = { Text(stringResource(id = R.string.PW_TEXT)) },
             singleLine = true,
         )
-
-        // 높이 30dp인 공간 추가
         Spacer(modifier = modifier.height(30.dp))
 
         // nickname
@@ -121,46 +120,41 @@ fun SignupScreen(
             placeholder = { Text(stringResource(id = R.string.NICKNAME_TEXT)) },
             singleLine = true,
         )
-
-        // 높이 30dp인 공간 추가
         Spacer(modifier = modifier.height(30.dp))
 
-        // mbti
+        // phone
         Text(
-            text = stringResource(id = R.string.MBTI),
+            text = stringResource(id = R.string.PHONE),
             fontSize = 20.sp,
             modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Left
         )
 
         TextField(
-            value = userMbti,
+            value = userPhone,
             onValueChange = { newValue ->
-                userMbti = newValue }, // 이 부분 안쓰면 입력안됨
+                userPhone = newValue }, // 이 부분 안쓰면 입력안됨
             modifier = modifier
                 .fillMaxWidth(),
-            label = { Text(stringResource(id = R.string.input_MBTI)) },
-            placeholder = { Text(stringResource(id = R.string.MBTI_TEXT)) },
+            label = { Text(stringResource(id = R.string.INPUT_PHONE)) },
+            placeholder = { Text(stringResource(id = R.string.PHONE_TEXT)) },
             singleLine = true,
         )
-
-        // 2 가중치 만큼 빈공간 추가
         Spacer(modifier = modifier.weight(2f))
 
         // signup button
-        val context = LocalContext.current
         AppButton(
             text = stringResource(id = R.string.SIGNUP),
             onClick = {
-                val user = getUserInfo(userId, userPw, userName, userMbti)
-                val validateSignUpData = signupViewModel.validateSignUp(user)
-                if(validateSignUpData == null){
-                    Toast.makeText(context, "회원가입성공", Toast.LENGTH_SHORT).show()
-                    signupViewModel.signUp(user)
-                    navController.navigate("login") {
-                        // BackStack 모두 비우고 home 이동.
-                        popUpTo("signup") { inclusive = true }
-                    }
+                signupViewModel.signUp(getSignUpRequestDto(userId, userPw, userName, userPhone))
+                Log.d("sign","$userId, $userPw, $userName, $userPhone")
+                if (signUpState.isSuccess) {
+                    Toast.makeText(context, signUpState.message, Toast.LENGTH_SHORT).show()
+                    // 회원가입 성공 후 로그인 페이지로 이동
+                    moveToLogin(signUpState.memberId!!, navController)
+                } else{
+                    // 실패 메시지 출력
+                    Toast.makeText(context, signUpState.message, Toast.LENGTH_SHORT).show()
                 }
             },
             padding = PaddingValues(vertical = 10.dp)
@@ -168,10 +162,19 @@ fun SignupScreen(
     }
 }
 
-private fun getUserInfo(userId: String?, userPw: String?, userName: String?, userMbti: String?): UserData = UserData(
-    id = userId.toString(),
-    pw = userPw.toString(),
-    name = userName.toString(),
-    mbti = userMbti.toString(),
-)
+private fun getSignUpRequestDto(userId:String, userPw:String, userName:String, userPhone: String): RequestSignUpDto {
+    return RequestSignUpDto(
+        authenticationId = userId,
+        password = userPw,
+        nickname = userName,
+        phone = userPhone
+    )
+}
 
+private fun moveToLogin(memberId: String, navController: NavController) {
+    val route = "login/$memberId"
+    navController.navigate(route) {
+        // SignUp 화면을 포함해 이전 모든 화면을 스택에서 제거
+        popUpTo("signup") { inclusive = true }
+    }
+}
