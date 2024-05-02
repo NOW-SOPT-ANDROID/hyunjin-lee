cb5a4bb1b40afa5f6b7408b6467aaf95629eb699
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-05-03 00:34:01 +0900

    [Feat] #27 User정보 API로 GET

---
+++ app/src/main/java/com/sopt/now/compose/MainViewModel.kt
@@ -0,0 +1,83 @@
+package com.sopt.now.compose
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import com.sopt.now.compose.data.auth.LoginData.ResponseLoginDto
+import com.sopt.now.compose.data.auth.ServicePool.authService
+import com.sopt.now.compose.data.auth.User.ResponseUserDto
+import com.sopt.now.compose.data.auth.User.UserState
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val _userInfo = MutableSharedFlow<UserState>()
+    val userInfo = _userInfo.asSharedFlow()
+
+    // 회원가입 정보 받아오기
+    fun getUserInfo(memberIdString: String) {
+        val memberId = memberIdString.toIntOrNull()?:0
+        authService.getUserInfo(memberId).enqueue(object : Callback<ResponseUserDto> {
+            override fun onResponse(
+                call: Call<ResponseUserDto>,
+                response: Response<ResponseUserDto>,
+            ) {
+                if (response.isSuccessful) {
+                    val data: ResponseUserDto? = response.body()
+                    val authenticationId = data?.data?.authenticationId
+
+                    // memberId를 통해 회원 정보 받아옴
+                    viewModelScope.launch {
+                        _userInfo.emit(
+                            UserState(
+                                isSuccess = true,
+                                message = "회원 조회 성공: ${data?.message}",
+                                userId = authenticationId
+                            )
+                        )
+                    }
+                } else {
+                    // 오류 응답 처리
+                    val error = response.errorBody()?.string()
+                    val gson = Gson()
+                    try {
+                        val errorResponse = gson.fromJson(error, ResponseLoginDto::class.java)
+                        viewModelScope.launch {
+                            _userInfo.emit(
+                                UserState(
+                                    isSuccess = false,
+                                    message = "회원 조회 실패: ${errorResponse.message}"
+                                )
+                            )
+                        }
+                    } catch (e: Exception) {
+                        viewModelScope.launch {
+                            _userInfo.emit(
+                                UserState(
+                                    isSuccess = false,
+                                    message = "회원 조회 실패: 에러 메시지 파싱 실패"
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseUserDto>, t: Throwable) {
+                viewModelScope.launch {
+                    _userInfo.emit(
+                        UserState(
+                            isSuccess = false,
+                            message = "서버 에러"
+                        )
+                    )
+                }
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/sopt/now/compose/data/auth/User/UserState.kt
@@ -0,0 +1,7 @@
+package com.sopt.now.compose.data.auth.User
+
+data class UserState(
+    val isSuccess: Boolean,
+    val message: String,
+    val userId: String? = null
+)
