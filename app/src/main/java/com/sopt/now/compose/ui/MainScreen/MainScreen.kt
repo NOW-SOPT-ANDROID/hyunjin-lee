e120d7a7ff2365ce093afe0b5b8f7365c9af10b1
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-05-03 00:55:22 +0900

    [Feat] #27 livedata -> flow로 수정

--- app/src/main/java/com/sopt/now/compose/ui/MainScreen/MainScreen.kt
+++ app/src/main/java/com/sopt/now/compose/ui/MainScreen/MainScreen.kt
@@ -2,7 +2,8 @@ package com.sopt.now.compose.ui.MainScreen
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.compose.rememberNavController
 import com.sopt.now.compose.MainScaffold
 import com.sopt.now.compose.Navigation.Navigation
@@ -10,16 +11,14 @@ import com.sopt.now.compose.ui.LoginScreen.LoginViewModel
 
 @Composable
 fun MainScreen(loginViewModel: LoginViewModel) {
-    val lifecycleOwner = LocalLifecycleOwner.current // LifecycleOwner를 얻습니다.
+    val isUserInfoAvailable by loginViewModel.loginstate.collectAsState(initial = false)
     val navController = rememberNavController()
 
     // LaunchedEffect를 사용하여 최초 한 번만 자동 로그인 확인
     LaunchedEffect(key1 = true) {
-        loginViewModel.userInfo.observe(lifecycleOwner) {
-            if (loginViewModel.userInfo.value == true) {
-                navController.navigate("home") {
-                    popUpTo("login") { inclusive = true }
-                }
+        if (isUserInfoAvailable as Boolean) {
+            navController.navigate("home") {
+                popUpTo("login") { inclusive = true }
             }
         }
     }
