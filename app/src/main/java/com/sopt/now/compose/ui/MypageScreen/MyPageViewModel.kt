af881f2f0e2df2805f230c5d839a7da81f4130c3
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-05-03 00:56:25 +0900

    [Feat] #27 MypageScreen 유저 정보 GET

--- app/src/main/java/com/sopt/now/compose/ui/MypageScreen/MyPageViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/ui/MypageScreen/MyPageViewModel.kt
@@ -1,28 +1,91 @@
 package com.sopt.now.compose.ui.MypageScreen
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
-import com.sopt.now.compose.MyApplication
-import com.sopt.now.compose.data.UserData
+import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import com.sopt.now.compose.data.auth.LoginData.LoginState
+import com.sopt.now.compose.data.auth.LoginData.ResponseLoginDto
+import com.sopt.now.compose.data.auth.ServicePool
+import com.sopt.now.compose.data.auth.User.ResponseUserDto
+import com.sopt.now.compose.data.auth.User.UserState
+import com.sopt.now.compose.data.mypage.MyPageState
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MyPageViewModel : ViewModel() {
-    private val _userInfo = MutableLiveData<UserData>()
+class MyPageViewModel(application: Application) : AndroidViewModel(application) {
+    // user 상태를 담은 flow
+    private val _myInfo = MutableSharedFlow<MyPageState>()
+    val myInfo = _myInfo.asSharedFlow()
 
-    init {
-        loadUserInfo()
-    }
+    // user 정보 받아오기
+    fun getUserInfo(memberIdString: String) {
+        val memberId = memberIdString.toIntOrNull()?:0
+        ServicePool.authService.getUserInfo(memberId).enqueue(object : Callback<ResponseUserDto> {
+            override fun onResponse(
+                call: Call<ResponseUserDto>,
+                response: Response<ResponseUserDto>,
+            ) {
+                if (response.isSuccessful) {
+                    val data: ResponseUserDto? = response.body()
+                    val authenticationId = data?.data?.authenticationId
+                    val nickname = data?.data?.nickname
+                    val phone = data?.data?.phone
 
-    private fun loadUserInfo() {
-        val userData = MyApplication.prefs.getUserData(PREF_KEY)
-        _userInfo.value = userData
-    }
+                    // memberId를 통해 회원 정보 받아옴
+                    viewModelScope.launch {
+                        _myInfo.emit(
+                            MyPageState(
+                                isSuccess = true,
+                                message = "회원 조회 성공: ${data?.message}",
+                                userId = authenticationId,
+                                userName = nickname,
+                                userPhone = phone
+                            )
+                        )
+                    }
+                } else {
+                    // 오류 응답 처리
+                    val error = response.errorBody()?.string()
+                    val gson = Gson()
+                    try {
+                        val errorResponse = gson.fromJson(error, ResponseLoginDto::class.java)
+                        viewModelScope.launch {
+                            _myInfo.emit(
+                                MyPageState(
+                                    isSuccess = false,
+                                    message = "회원 조회 실패: ${errorResponse.message}"
+                                )
+                            )
+                        }
+                    } catch (e: Exception) {
+                        viewModelScope.launch {
+                            _myInfo.emit(
+                                MyPageState(
+                                    isSuccess = false,
+                                    message = "회원 조회 실패: 에러 메시지 파싱 실패"
+                                )
+                            )
+                        }
+                    }
+                }
+            }
 
-    fun getUserInfo(): UserData {
-        return MyApplication.prefs.getUserData(PREF_KEY)
-    }
-
-    companion object {
-        private const val PREF_KEY = "userData"
+            override fun onFailure(call: Call<ResponseUserDto>, t: Throwable) {
+                viewModelScope.launch {
+                    _myInfo.emit(
+                        MyPageState(
+                            isSuccess = false,
+                            message = "서버 에러"
+                        )
+                    )
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/sopt/now/compose/ui/MypageScreen/MypageScreen.kt
+++ app/src/main/java/com/sopt/now/compose/ui/MypageScreen/MypageScreen.kt
@@ -1,5 +1,6 @@
 package com.sopt.now.compose.ui.MypageScreen
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,9 +12,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -21,9 +28,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.sopt.now.compose.MyApplication
 import com.sopt.now.compose.R
 import com.sopt.now.compose.component.AppButton
+import com.sopt.now.compose.data.mypage.MyPageState
 
 @Composable
 fun MypageScreen(
@@ -31,7 +38,23 @@ fun MypageScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
 ) {
-    val userData = myPageViewModel.getUserInfo()
+    val context = LocalContext.current
+
+    // 사용자 정보 상태 관찰
+    val myPageState by myPageViewModel.myInfo.collectAsState(initial = MyPageState(isSuccess = false, ""))
+
+    var userId by rememberSaveable { mutableStateOf("") }
+    var userName by rememberSaveable { mutableStateOf("") }
+    var userPhone by rememberSaveable { mutableStateOf("") }
+
+    if (myPageState.isSuccess) {
+        Toast.makeText(context, myPageState.message, Toast.LENGTH_SHORT).show()
+        userId = myPageState.userId ?: ""
+        userName = myPageState.userName ?: ""
+        userPhone = myPageState.userPhone ?: ""
+    } else if (myPageState.message.isNotEmpty()) {
+        Toast.makeText(context, myPageState.message, Toast.LENGTH_SHORT).show()
+    }
 
     Column(
         modifier = modifier
@@ -47,8 +70,6 @@ fun MypageScreen(
             modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Center
         )
-
-        // 높이 30dp인 공간 추가
         Spacer(modifier = modifier.height(50.dp))
 
         val painter = painterResource(id = R.drawable.ic_profile_img)
@@ -70,13 +91,11 @@ fun MypageScreen(
         )
 
         Text(
-            text = userData.id,
+            text = userId,
             fontSize = 20.sp,
             modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Left
         )
-
-        // 높이 30dp인 공간 추가
         Spacer(modifier = modifier.height(30.dp))
 
         // pw
@@ -88,13 +107,11 @@ fun MypageScreen(
         )
 
         Text(
-            text = userData.pw,
+            text = userName,
             fontSize = 20.sp,
             modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Left
         )
-
-        // 높이 30dp인 공간 추가
         Spacer(modifier = modifier.height(30.dp))
 
         // nickname
@@ -106,25 +123,16 @@ fun MypageScreen(
         )
 
         Text(
-            text = userData.name,
+            text = userPhone,
             fontSize = 20.sp,
             modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Left
         )
-
-        // 높이 30dp인 공간 추가
         Spacer(modifier = modifier.height(30.dp))
 
         // mbti
         Text(
-            text = stringResource(id = R.string.MBTI),
-            fontSize = 20.sp,
-            modifier = modifier.fillMaxWidth(),
-            textAlign = TextAlign.Left
-        )
-
-        Text(
-            text = userData.mbti,
+            text = stringResource(id = R.string.PHONE),
             fontSize = 20.sp,
             modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Left
@@ -135,7 +143,6 @@ fun MypageScreen(
         AppButton(
             text = stringResource(id = R.string.LOGOUT),
             onClick = {
-                MyApplication.prefs.clearUserData()
                 navController.navigate("login") {
                     popUpTo("mypage") { inclusive = true }
                 }
