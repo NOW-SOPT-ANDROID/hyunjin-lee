82f5a1ab01b2d8cfde59636b38dbe0086d3c1c7b
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-05-03 00:30:54 +0900

    [Feat] #27 LoginScreen API 연결

--- app/src/main/java/com/sopt/now/compose/ui/LoginScreen/LoginScreen.kt
+++ app/src/main/java/com/sopt/now/compose/ui/LoginScreen/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.sopt.now.compose.ui.LoginScreen
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,6 +12,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,24 +27,51 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
+import com.sopt.now.compose.MainViewModel
 import com.sopt.now.compose.R
 import com.sopt.now.compose.component.AppButton
+import com.sopt.now.compose.data.auth.LoginData.LoginState
+import com.sopt.now.compose.data.auth.LoginData.RequestLoginDto
+import com.sopt.now.compose.data.auth.User.UserState
 import com.sopt.now.compose.util.modifier.noRippleClickable
 
 @Composable
 fun LoginScreen(
     loginViewModel: LoginViewModel,
-    modifier: Modifier = Modifier,
+    mainViewModel: MainViewModel,
     navController: NavController,
+    navBackStackEntry: NavBackStackEntry
 ) {
     val context = LocalContext.current
     var expanded = remember { mutableStateOf(false) }
-    val userData = loginViewModel.getUserInfo()
+
+    // 로그인 상태 관찰
+    val loginState by loginViewModel.loginstate.collectAsState(initial = LoginState(false, ""))
+
+    // 사용자 정보 상태 관찰
+    val userState by mainViewModel.userInfo.collectAsState(initial = UserState(isSuccess = false, ""))
 
     // 기본값으로 회원가입에서 받아온 값을 사용하되, 사용자 입력을 통해 업데이트될 수 있도록 remember 사용
-    var userId by rememberSaveable { mutableStateOf(userData.id) }
-    var userPw by rememberSaveable { mutableStateOf(userData.pw) }
+    var userId by rememberSaveable { mutableStateOf("") }
+    var userPw by rememberSaveable { mutableStateOf("") }
+
+    // 회원 정보 조회
+    val memberId = navBackStackEntry.arguments?.getString("memberId") ?: ""
+    Log.d("loginscreen", memberId)
+    LaunchedEffect(memberId) {
+        mainViewModel.getUserInfo(memberId)
+        if (userState.isSuccess) {
+            // 사용자 아이디를 EditText에 설정
+            userId = userState.userId ?: ""
+            // 회원 조회 성공 메시지 표시
+            Toast.makeText(context, userState.message, Toast.LENGTH_SHORT).show()
+        } else {
+            // 회원 조회 실패 메시지 표시
+            Toast.makeText(context, userState.message, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     Column(
         modifier = Modifier.noRippleClickable {
@@ -59,7 +89,6 @@ fun LoginScreen(
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp
         )
-
         Spacer(modifier = Modifier.weight(0.5f))
 
         // id
@@ -76,8 +105,6 @@ fun LoginScreen(
             placeholder = { Text(stringResource(id = R.string.ID_daisy)) },
             singleLine = true,
         )
-
-        // 높이 30dp인 공간 추가
         Spacer(modifier = Modifier.height(30.dp))
 
         // pw
@@ -95,13 +122,12 @@ fun LoginScreen(
             singleLine = true,
             visualTransformation = PasswordVisualTransformation(), // 비밀번호 마스킹
         )
-
         Spacer(modifier = Modifier.weight(1.0f))
 
         // signup button
         AppButton(
             text = stringResource(id = R.string.SIGNUP),
-            onClick = {
+            onClick = { // 회원가입 버튼 클릭시
                 Toast.makeText(context, R.string.SIGNUP, Toast.LENGTH_SHORT).show()
                 navController.navigate("signup") {
                     popUpTo("login") { inclusive = true }
@@ -109,30 +135,32 @@ fun LoginScreen(
             },
             padding = PaddingValues(vertical = 10.dp)
         )
-
         Spacer(modifier = Modifier.height(15.dp))
 
         // login button
         AppButton(
             text = stringResource(id = R.string.LOGIN),
-            onClick = {
-                val(inputId, inputPw) = Pair(
-                    userData.id,
-                    userData.pw
-                )
+            onClick = { // 로그인 버튼 클릭시
+                loginViewModel.login(getLoginRequestDto(userId, userPw))
 
-                if (loginViewModel.loginValid(inputId, inputPw, userData)) {
-                    Toast.makeText(context, "로그인 성공", Toast.LENGTH_SHORT).show()
+                if (loginState.isSuccess) {
+                    Toast.makeText(context, loginState.message, Toast.LENGTH_SHORT).show()
                     navController.navigate("home") {
                         popUpTo("login") { inclusive = true }
                     }
-                } else {
-                    Toast.makeText(context, "로그인 실패", Toast.LENGTH_SHORT).show()
+                } else if (loginState.message.isNotEmpty()) {
+                    Toast.makeText(context, loginState.message, Toast.LENGTH_SHORT).show()
                 }
             },
             padding = PaddingValues(vertical = 10.dp)
         )
-
         Spacer(modifier = Modifier.weight(0.5f))
     }
+}
+
+fun getLoginRequestDto(userId: String, userPw: String): RequestLoginDto {
+    return RequestLoginDto(
+        authenticationId = userId,
+        password = userPw,
+    )
 }
